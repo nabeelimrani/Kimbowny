7714a141ba5471d13f84f90f859d6f2aafb28dc0
Syed Hassan Raza Rezve <rezveshah1211@gmail.com>
2024-01-12 14:42:05 +0500

    another commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
+                        asd
                     {{ __('You are logged in!') }}
                 </div>
             </div>
